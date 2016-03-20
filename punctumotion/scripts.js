e1c5d647c053ae9ef0169599554988573ba38c7b
kaideas <kai.takahashi@gmail.com>
2016-03-19 23:54:03 -0600

    punctumotion

---
+++ punctumotion/scripts.js
@@ -0,0 +1,42 @@
+function pasteHtmlAtCaret(html) {
+    var sel, range;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+
+            var el = document.createElement("div");
+            el.innerHTML = html;
+            var frag = document.createDocumentFragment(), node, lastNode;
+            while ( (node = el.firstChild) ) {
+                lastNode = frag.appendChild(node);
+            }
+            var firstNode = frag.firstChild;
+            range.insertNode(frag);
+            
+            // Preserve the selection
+            if (lastNode) {
+                range = range.cloneRange();
+                range.setStartAfter(lastNode);
+                range.collapse(true);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+        }
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        // IE < 9
+        var originalRange = sel.createRange();
+        originalRange.collapse(true);
+        sel.createRange().pasteHTML(html);
+    }
+}
+window.onload = function(){ 
+ 
+    document.getElementById('nervous').onclick = function() {
+        document.getElementById('text').focus();
+        pasteHtmlAtCaret('<span class="nervous punctumotion">.</span>&nbsp;'); //need to move cursor forward to replace &nbsp;
+        return false;
+    };
+};
