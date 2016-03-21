b14fcfb4fde1894633430e1157b138bade774ae0
kaideas <kai.takahashi@gmail.com>
2016-03-20 23:09:38 -0600

    fix send

--- punctumotion/messages.php
+++ punctumotion/messages.php
@@ -1,1 +1,15 @@
-messages.php
+<?php
+
+
+($_SERVER["REQUEST_METHOD"] == "POST")
+
+$result = CS50::query("INSERT IGNORE INTO messages (message) 
+            VALUES(?)", $_POST["message"], 
+
+                $rows = CS50::query("SELECT LAST_INSERT_ID() AS id");
+                $id = $rows[0]["id"];
+                $_SESSION["id"] = $id;
+                redirect("/display.php");
+            }
+
+?>
